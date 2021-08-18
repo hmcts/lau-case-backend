cab2995b0e1fd810506a628e7990d37314a10f86
Martin <martin.spasov@hmcts.net>
2021-08-18 17:17:13 +0100

    Test commit

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/security/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html",
+         web.ignoring().antMatchers("/swagger-ui.html",
                                    "/webjars/springfox-swagger-ui/**",
                                    "/swagger-resources/**",
                                    "/health",
