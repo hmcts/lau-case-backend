2a1c609811184937d6c0c11d4314bb7fa2518454
Dave Jones <dave.jones@hmcts.net>
2021-10-12 17:21:41 +0100

    LAU-151: Make LAU database available in Preview Environment

--- src/main/resources/db/migration/V001__Schema.sql
+++ src/main/resources/db/migration/V001__Schema.sql
@@ -59,7 +59,17 @@
   CREATE INDEX case_search_audit_cases_search_id_idx ON case_search_audit_cases (search_id);
 
   -- create user for application access
-  CREATE USER lauuser WITH PASSWORD '${LAU_DB_PASSWORD}';
+  DO
+  $do$
+  BEGIN
+     IF NOT EXISTS (
+        SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
+        WHERE  rolname = 'lauuser') THEN
+        CREATE ROLE lauuser LOGIN PASSWORD '${LAU_DB_PASSWORD}';
+     END IF;
+  END
+  $do$;
+
   GRANT USAGE, SELECT ON SEQUENCE case_view_audit_case_view_id_seq TO lauuser;
   GRANT USAGE, SELECT ON SEQUENCE case_search_audit_id_seq TO lauuser;
   GRANT USAGE, SELECT ON SEQUENCE case_search_audit_cases_id_seq TO lauuser;
