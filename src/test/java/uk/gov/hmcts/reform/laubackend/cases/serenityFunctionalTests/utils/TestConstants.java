a31befd459d19914fd10dc35ba13fcb2d745d50a
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2021-10-13 11:03:38 +0100

    Test commit

---
+++ src/test/java/uk/gov/hmcts/reform/laubackend/cases/serenityFunctionalTests/utils/TestConstants.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.laubackend.cases.serenityFunctionalTests.utils;
+
+public final class TestConstants {
+
+    public static final String BASE_URL = "http://lau-case-backend-aat.service.core-compute-aat.internal";
+    public static final String S2S_NAME = "lau_case_frontend";
+    public static final String S2S_SECRET = "";
+    public static final String S2S_URL = "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal/testing-support";
+
+    /*CaseViw endPoint*/
+    public static final String AUDIT_CASE_VIEW_ENDPOINT = "/audit/caseView";
+}
