39d172cdd5b4424e7391f354f2557d63e540216a
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2021-10-15 18:28:48 +0100

    Adjusted @Ignore Tag

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/controllers/EndpointSecurityTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/controllers/EndpointSecurityTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("functional")
 public class EndpointSecurityTest extends LauCaseBaseFunctionalTest {
 
-    @Test @Ignore
+    @Ignore
     public void should_allow_unauthenticated_requests_to_welcome_message_and_return_200_response_code() {
 
         String response = lauCaseBackEndServiceClient.getWelcomePage();
