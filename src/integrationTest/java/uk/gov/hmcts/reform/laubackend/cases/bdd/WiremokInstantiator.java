ad3dd5c703f7cbc2712fa38177708f06954bded8
Martin <martin.spasov@hmcts.net>
2021-11-29 10:59:21 +0000

    Add idam env var configs

--- src/integrationTest/java/uk/gov/hmcts/reform/laubackend/cases/bdd/WiremokInstantiator.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/laubackend/cases/bdd/WiremokInstantiator.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.laubackend.cases.bdd;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import lombok.Getter;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 @Getter
 public enum WiremokInstantiator {
     INSTANCE;
@@ -10,6 +12,11 @@ public enum WiremokInstantiator {
     private final WireMockServer wireMockServer = new WireMockServer(4554);
 
     WiremokInstantiator() {
-        wireMockServer.start();
+        try {
+            wireMockServer.start();
+            SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            //DO NOTHING
+        }
     }
 }
