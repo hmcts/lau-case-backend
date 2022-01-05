c90b0cf55ef429a61e6890a327fb095543b6ae5b
RababRabab <Rabab.Rabab1@hmcts.net>
2022-01-05 19:31:30 +0000

    swagger atutomation test via integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/laubackend/cases/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/laubackend/cases/config/SwaggerPublisherTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.reform.laubackend.cases.authorization.AuthService;
-import uk.gov.hmcts.reform.laubackend.cases.authorization.AuthorisedServices;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -30,12 +27,6 @@ class SwaggerPublisherTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @MockBean
-    private AuthService authService;
-
-    @MockBean
-    private AuthorisedServices authorisedServices;
 
     @DisplayName("Generate swagger documentation")
     @Test
