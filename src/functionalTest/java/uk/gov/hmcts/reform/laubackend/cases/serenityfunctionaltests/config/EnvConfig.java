df56372c30de4965c60b63bde7739ceedc15caf2
Martin <martin.spasov@hmcts.net>
2022-01-12 15:41:03 +0000

    LAU-237 Move Idam username and password to Azure vault

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/serenityfunctionaltests/config/EnvConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/serenityfunctionaltests/config/EnvConfig.java
@@ -4,6 +4,8 @@ public final class EnvConfig {
 
     public static final String API_URL = System.getenv("TEST_URL");
     public static final String IDAM_CLIENT_SECRET = System.getenv("IDAM_CLIENT_SECRET");
+    public static final String IDAM_USERNAME = System.getenv("IDAM_USERNAME");
+    public static final String IDAM_PASSWORD = System.getenv("IDAM_PASSWORD");
 
 
     private EnvConfig() {
