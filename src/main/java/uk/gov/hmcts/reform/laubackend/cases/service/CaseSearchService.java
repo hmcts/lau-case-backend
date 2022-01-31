d6515d92ade82801926d1ebb783ac370da6d2728
Martin <martin.spasov@hmcts.net>
2022-01-31 11:09:56 +0000

    Add logging for performance testing issues.

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/service/CaseSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/service/CaseSearchService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.laubackend.cases.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,11 +23,13 @@ import java.util.Optional;
 
 import static java.lang.Integer.parseInt;
 import static java.lang.Long.valueOf;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.springframework.data.domain.PageRequest.of;
 import static org.springframework.util.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.reform.laubackend.cases.response.CaseSearchGetResponse.caseSearchResponse;
 
 @Service
+@Slf4j
 public class CaseSearchService {
 
     @Autowired
@@ -37,6 +40,9 @@ public class CaseSearchService {
 
     public CaseSearchGetResponse getCaseSearch(final SearchInputParamsHolder inputParamsHolder) {
 
+        final long getCaseRepositoryStartTime = System.currentTimeMillis();
+        log.info("Case search REPOSITORY invoked: " + getCaseRepositoryStartTime);
+
         final Page<CaseSearchAudit> caseSearch = caseSearchAuditRepository.findCaseSearch(
                 inputParamsHolder.getUserId(),
                 inputParamsHolder.getCaseRef(),
@@ -44,6 +50,16 @@ public class CaseSearchService {
                 timestampUtil.getTimestampValue(inputParamsHolder.getEndTime()),
                 getPage(inputParamsHolder.getSize(), inputParamsHolder.getPage())
         );
+
+        final long getCaseRepositoryEndTime = System.currentTimeMillis();
+        final long totalTime = getCaseRepositoryEndTime - getCaseRepositoryStartTime;
+        log.info("Case search REPOSITORY finished: " + getCaseRepositoryEndTime);
+        log.info("Case search REPOSITORY total invocation time: " + totalTime + " milliseconds");
+        log.info("Case search REPOSITORY total invocation time: " + MILLISECONDS.toSeconds(totalTime) + " seconds");
+        log.info("**************************************************************************");
+
+        long getCaseObjectCreationStartTime = System.currentTimeMillis();
+        log.info("Case search OBJECT CREATION invoked: " + getCaseObjectCreationStartTime);
 
         final List<SearchLog> searchLogList = new ArrayList<>();
 
@@ -53,6 +69,14 @@ public class CaseSearchService {
                     new SearchLog().toDto(caseSearchAudit, timestamp)
             );
         });
+
+        final long getCaseObjectCreationEndTime = System.currentTimeMillis();
+        final long totalObjectCreationTime = getCaseObjectCreationEndTime - getCaseObjectCreationStartTime;
+        log.info("Case search OBJECT CREATION finished: " + getCaseObjectCreationEndTime);
+        log.info("Case search OBJECT CREATION total invocation time: " + totalObjectCreationTime + " milliseconds");
+        log.info("Case search OBJECT CREATION total invocation time: "
+                + MILLISECONDS.toSeconds(totalObjectCreationTime) + " seconds");
+        log.info("**************************************************************************");
 
         return caseSearchResponse()
                 .withSearchLog(searchLogList)
