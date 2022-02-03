8de7077a9db9222119e3e251d2bbdf5fc2a42343
Dave Jones <dave.jones@hmcts.net>
2022-02-03 17:36:50 +0000

    LAU-258: Updated code to remove citext.

--- src/main/resources/db/migration/V005__Schema.sql
+++ src/main/resources/db/migration/V005__Schema.sql
@@ -1,6 +1,19 @@
 ALTER TABLE case_action_audit ALTER COLUMN case_type_id TYPE VARCHAR(70);
 ALTER TABLE case_action_audit ALTER COLUMN case_jurisdiction_id TYPE VARCHAR(70);
 
-DROP EXTENSION IF EXISTS citext;
+-- drop extension safely
+DO
+$do$
+BEGIN
+  IF EXISTS (
+  SELECT FROM pg_extension  -- SELECT list can be empty for this
+    WHERE extname = 'citext') THEN
+  DROP EXTENSION IF EXISTS citext;
+END IF;
+exception
+  when sqlstate '42501' then
+  RAISE NOTICE 'Dropping citext failed.';
+END
+$do$;
 
 
