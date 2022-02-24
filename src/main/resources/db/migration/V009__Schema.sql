244f0d56830c4eaa2041f84402a94f420f3ff1d2
Dave Jones <38425925+davejones74@users.noreply.github.com>
2022-02-24 19:31:53 +0000

    LAU-307: Testing migration with env value. (#79)
    * LAU-307: Testing migration with env value.
    
    * LAU-307: Testing migration with env value.
    
    * LAU-307: Testing migration with env value.

---
+++ src/main/resources/db/migration/V009__Schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.case_action_audit(
+	case_action_id, user_id, case_action, case_ref, case_jurisdiction_id, case_type_id, log_timestamp)
+	VALUES (510101010, 'a2afa42c-20e1-41fb-99e5-00946e7b01d0', 'VIEW', '1643794917732066', '${LAU_DB_DUMMY}', 'GRANTOFREPRESENTATION', '2022-02-02 17:45:20.282063');
