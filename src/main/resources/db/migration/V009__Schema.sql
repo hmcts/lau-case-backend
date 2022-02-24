1c4ab06d91816563418c52299b588a8ffa0ed118
Dave Jones <dave.jones@hmcts.net>
2022-02-24 21:13:05 +0000

    LAU-307: Testing migration with env value.

--- src/main/resources/db/migration/V009__Schema.sql
+++ src/main/resources/db/migration/V009__Schema.sql
@@ -1,3 +1,11 @@
-INSERT INTO public.case_action_audit(
-	case_action_id, user_id, case_action, case_ref, case_jurisdiction_id, case_type_id, log_timestamp)
-	VALUES (510101010, 'a2afa42c-20e1-41fb-99e5-00946e7b01d0', 'VIEW', '1643794917732066', '${LAU_DB_DUMMY}', 'GRANTOFREPRESENTATION', '2022-02-02 17:45:20.282063');
+  -- alter role for application access
+  DO
+  $do$
+  BEGIN
+     IF NOT EXISTS (
+        SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
+        WHERE  rolname = 'lauuser') THEN
+        ALTER ROLE lauuser LOGIN PASSWORD '${LAU_DB_PASSWORD}';
+     END IF;
+  END
+  $do$;
