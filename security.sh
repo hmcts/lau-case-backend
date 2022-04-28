d8a2d292ae0eede026fa24ffe9a0337f1076a99d
Dave Jones <dave.jones@hmcts.net>
2022-04-28 13:45:48 +0100

    LAU-89: Secrity scan changes.

--- security.sh
+++ security.sh
@@ -7,5 +7,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 echo 'Zap scan not run' > /zap/api-report.html
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
-mkdir -p functional-output && cp /zap/api-report.html functional-output/
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
+mkdir -p functional-output
+chmod a+wx functional-output
+cp /zap/api-report.html functional-output/
