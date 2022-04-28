99183631f41dd863bbddfca96633c356e78047aa
Dave Jones <dave.jones@hmcts.net>
2022-04-28 13:29:45 +0100

    LAU-89: Secrity scan changes.

--- security.sh
+++ security.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-cat zap.out
 echo "ZAP has successfully started"
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 export LC_ALL=C.UTF-8
@@ -10,4 +9,3 @@ echo 'Zap scan not run' > /zap/api-report.html
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
 mkdir -p functional-output && cp /zap/api-report.html functional-output/
-cp *.* functional-output/
