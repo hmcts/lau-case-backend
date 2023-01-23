dde6bedf7bad16cc708c5d671ed943ca8f62cf5b
Martin Spasov <martin.y.spasov@gmail.com>
2023-01-23 13:04:04 +0000

    LAU-591 Remove unnecessary CaseRef logs (#209)

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/utils/CaseRefsUtils.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/utils/CaseRefsUtils.java
@@ -15,23 +15,17 @@ public final class CaseRefsUtils {
     }
 
     public static List<String> cleanUpCaseRefList(final List<String> caseRefs) {
-        log.info("Incoming caseRef list for saveCaseSearch: {}", caseRefs);
 
         caseRefs.removeIf(caseRef -> caseRef == null || caseRef.equals("") || caseRef.matches("^[a-zA-Z]*$"));
         caseRefs.replaceAll(s -> s.replaceAll("\\D+", ""));
-
-        log.info("CaseRef list after clean up for saveCaseSearch: {}", caseRefs);
 
         return caseRefs;
     }
 
     public static String cleanUpCaseRef(final String caseRef) {
-        log.info("Incoming caseRef for saveCaseAction: {}", caseRef);
 
         final String cleanCaseRef = caseRef.replaceAll("\\D+", "");
         final boolean isValidCaseRef = compile(CASE_REF_REGEX).matcher(cleanCaseRef).matches();
-
-        log.info("CaseRef after clean up for saveCaseAction: {}", cleanCaseRef);
 
         return isValidCaseRef ? cleanCaseRef : null;
     }
