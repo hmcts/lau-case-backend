be1369bc459e6dd9a41dcbbd623bd305b6cbf3cc
Priyanka Verma <priyanka.verma@hmcts.net>
2023-02-09 14:24:35 +0000

    LAU-588 suppress waring for final param in repository

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/repository/CaseSearchAuditRepository.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/repository/CaseSearchAuditRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.laubackend.cases.domain.CaseSearchAudit;
 
 import java.sql.Timestamp;
 
-@SuppressWarnings({"PMD.UseObjectForClearerAPI","PMD.UnnecessaryAnnotationValueElement","PMD.AvoidDuplicateLiterals"})
+@SuppressWarnings({"PMD.UseObjectForClearerAPI","PMD.UnnecessaryAnnotationValueElement",
+    "PMD.AvoidDuplicateLiterals","PMD.FinalParameterInAbstractMethod"})
 @Repository
 public interface CaseSearchAuditRepository extends JpaRepository<CaseSearchAudit, Long> {
 
@@ -32,11 +34,11 @@ public interface CaseSearchAuditRepository extends JpaRepository<CaseSearchAudit
             +   "OR cs.log_timestamp <= cast(cast(:endTime as varchar) as timestamp)) "
             + "limit 10000) cs",
         nativeQuery = true)
-    Page<CaseSearchAudit> findCaseSearch(String userId,
-                                                   String caseRef,
-                                                   Timestamp startTime,
-                                                   Timestamp endTime,
-                                                   Pageable pageable);
+    Page<CaseSearchAudit> findCaseSearch(final @Param("userId") String userId,
+                                         final @Param("caseRef") String caseRef,
+                                         final @Param("startTime") Timestamp startTime,
+                                         final @Param("endTime") Timestamp endTime,
+                                         final Pageable pageable);
 
     @Query(value = "SELECT cs.id, cs.user_id, cs.log_timestamp, cs.case_refs FROM case_search_audit cs "
         + "WHERE (cast(:userId as text) IS NULL OR cs.user_id=cast(:userId as text)) "
@@ -57,9 +59,9 @@ public interface CaseSearchAuditRepository extends JpaRepository<CaseSearchAudit
             +   "OR cs.log_timestamp <= cast(cast(:endTime as varchar) as timestamp)) "
             + "limit 10000) cs",
         nativeQuery = true)
-    Page<CaseSearchAudit> findCaseSearchH2(String userId,
-                                         String caseRef,
-                                         Timestamp startTime,
-                                         Timestamp endTime,
-                                         Pageable pageable);
+    Page<CaseSearchAudit> findCaseSearchH2(final @Param("userId") String userId,
+                                           final @Param("caseRef") String caseRef,
+                                           final @Param("startTime") Timestamp startTime,
+                                           final @Param("endTime") Timestamp endTime,
+                                           final Pageable pageable);
 }
