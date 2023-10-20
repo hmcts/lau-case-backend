f703e9b5b39f6ad7c37fc09146f0edf6bf15e9a3
Martin <martin.spasov@hmcts.net>
2023-10-20 14:16:33 +0100

    Create lau-case flexible server

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,20 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  subscription_id            = var.aks_subscription_id
+  skip_provider_registration = "true"
+  features {}
+  alias                      = "postgres_network"
+
+}
+
+
 locals {
   db_connection_options  = "?sslmode=require"
   vault_name             = "${var.product}-${var.env}"
   asp_name               = "${var.product}-${var.env}"
+  env                    = var.env
 }
 
 module "lau-case-db" {
@@ -23,6 +33,40 @@ module "lau-case-db" {
   subscription           = var.subscription
 }
 
+
+module "lau-case-db-flexible" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env = var.env
+
+  product = var.product
+  component = var.component
+  business_area = "cft"
+  name = "${var.product}-${var.component}-flexible"
+
+  common_tags = var.common_tags
+
+  pgsql_admin_username = "lauadmin"
+  pgsql_version   = "15"
+
+  pgsql_databases = [
+    {
+      name: "lau_case"
+    }
+  ]
+
+  pgsql_server_configuration = [
+    {
+      name = "azure.extensions"
+      value = "plpgsql,pg_stat_statements,pg_buffercache,pgcrypto,hypopg"
+    }
+  ]
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+}
 data "azurerm_key_vault" "key_vault" {
   name                = local.vault_name
   resource_group_name = local.vault_name
@@ -68,4 +112,33 @@ resource "azurerm_key_vault_secret" "flyway_password" {
   name         = "flyway-password"
   value        = module.lau-case-db.postgresql_password
 }
+
+///////////////////////////////////////
+// Populate Vault with Flexible DB info
+//////////////////////////////////////
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER-FLEXIBLE" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-USER-FLEXIBLE"
+  value        = module.lau-case-db-flexible.username
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS-FLEXIBLE" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-PASS-FLEXIBLE"
+  value        = module.lau-case-db-flexible.password
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST_FLEXIBLE" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-HOST-FLEXIBLE"
+  value        = module.lau-case-db-flexible.fqdn
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT_FLEXIBLE" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name      = "${var.component}-POSTGRES-PORT-FLEXIBLE"
+  value     =  var.postgresql_flexible_server_port
+}
+
 
