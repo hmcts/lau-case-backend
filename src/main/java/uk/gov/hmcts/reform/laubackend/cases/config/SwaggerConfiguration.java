b30cb12375a98fa0c39257b6e9af9aeab4d39772
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-05-31 08:43:25 +0100

    address spring boot 3.3.0 issues

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/config/SwaggerConfiguration.java
@@ -3,26 +3,8 @@ package uk.gov.hmcts.reform.laubackend.cases.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.flywaydb.core.internal.util.StringUtils;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
-import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
-import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
-import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
-import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
-import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -37,34 +19,5 @@ public class SwaggerConfiguration {
             .license(licence);
 
         return new OpenAPI().info(info);
-    }
-
-    @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
-                     ServletEndpointsSupplier servletEndpointsSupplier,
-                     ControllerEndpointsSupplier controllerEndpointsSupplier,
-                     EndpointMediaTypes endpointMediaTypes,
-                     CorsEndpointProperties corsProperties,
-                     WebEndpointProperties webEndpointProperties,
-                     Environment environment) {
-        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-        allEndpoints.addAll(webEndpoints);
-        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-        String basePath = webEndpointProperties.getBasePath();
-        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties,
-                                                                             environment, basePath);
-        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
-                        corsProperties.toCorsConfiguration(),
-                        new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping);
-    }
-
-
-    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
-                                               Environment environment, String basePath) {
-        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
-            || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
