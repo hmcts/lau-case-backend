4e175476104f7bb2a2eacbb078614d683bdd3aff
Priyanka Verma <priyanka.verma@hmcts.net>
2024-09-19 13:42:42 +0100

    Fortify fix

--- bin/set-lau-docker-env.sh
+++ bin/set-lau-docker-env.sh
@@ -7,15 +7,15 @@ export LAU_DB_NAME=lau_case
 export LAU_DB_HOST=0.0.0.0
 export LAU_DB_PORT=5054
 export LAU_DB_USERNAME=lauuser
-export LAU_DB_PASSWORD=laupass
+export LAU_DB_PASSWORD=lau1234
 export LAU_DB_ADMIN_USERNAME=lauadmin
-export LAU_DB_ADMIN_PASSWORD=laupass
+export LAU_DB_ADMIN_PASSWORD=lau1234
 export CASE_BACKEND_ENCRYPTION_KEY=my_very_secure_key
 
 export FLYWAY_PLACEHOLDERS_LAU_DB_USERNAME=lauuser
-export FLYWAY_PLACEHOLDERS_LAU_DB_PASSWORD=laupass
+export FLYWAY_PLACEHOLDERS_LAU_DB_PASSWORD=lau1234
 export FLYWAY_URL=jdbc:postgresql://0.0.0.0:5054/lau_case
 export FLYWAY_USER=lauadmin
-export FLYWAY_PASSWORD=laupass
+export FLYWAY_PASSWORD=lau1234
 export FLYWAY_NOOP_STRATEGY=false
 
