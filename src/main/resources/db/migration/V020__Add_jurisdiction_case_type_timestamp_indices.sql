c0536837a7c111dc40c98c011bab05530448d2cd
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-02-07 15:38:59 +0000

    Whitespace

--- src/main/resources/db/migration/V020__Add_jurisdiction_case_type_timestamp_indices.sql
+++ src/main/resources/db/migration/V020__Add_jurisdiction_case_type_timestamp_indices.sql
@@ -4,4 +4,4 @@ ON case_action_audit (log_timestamp, case_jurisdiction_id);
 
 CREATE INDEX concurrently IF NOT EXISTS
   case_action_audit_timestamp_case_type_idx
-    ON case_action_audit (log_timestamp, case_type_id);
+ON case_action_audit (log_timestamp, case_type_id);
