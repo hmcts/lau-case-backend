5e35229bf49d0dae3514dbab86500db196f3af16
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-06-25 10:02:09 +0100

    Import block added for 'lau-case-backend-flexible-prod' DB server

---
+++ infrastructure/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server.pgsql_server
+  id = "/subscriptions/8999dec3-0104-4a27-94ee-6588559729d1/resourceGroups/lau-case-backend-flexible-data-prod/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-prod"
+}
