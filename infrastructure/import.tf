d10845c0f03314e51f3ec654940b9f5e81d2c661
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-06-30 12:54:45 +0100

    Import blocks for aat DB server only added

--- infrastructure/import.tf
+++ infrastructure/import.tf
@@ -1,15 +1,15 @@
-# AAT environment imports - only active when env=aat
+# AAT environment imports
 import {
-  to = var.env == "aat" ? module.lau-case-db-flexible.azurerm_postgresql_flexible_server.pgsql_server : null
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server.pgsql_server
   id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-aat/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-aat"
 }
 
 import {
-  to = var.env == "aat" ? module.lau-case-db-flexible.azurerm_postgresql_flexible_server_database.pg_databases["lau_case"] : null
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server_database.pg_databases["lau_case"]
   id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-aat/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-aat/databases/lau_case"
 }
 
 import {
-  to = var.env == "aat" ? module.lau-case-db-flexible.azurerm_postgresql_flexible_server_configuration.pgsql_server_config["azure.extensions"] : null
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server_configuration.pgsql_server_config["azure.extensions"]
   id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-aat/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-aat/configurations/azure.extensions"
 }
