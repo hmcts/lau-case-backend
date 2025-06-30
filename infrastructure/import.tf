d7a2b6b0d5d0f138ba960333b2b90adc3ffc95f7
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-06-30 09:48:44 +0100

    Import of 'lau-case-backend-flexible-demo' to tf state file

---
+++ infrastructure/import.tf
@@ -0,0 +1,14 @@
+import {
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server.pgsql_server
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-demo/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-demo"
+}
+
+import {
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server_database.pg_databases["lau_case"]
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-demo/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-demo/databases/lau_case"
+}
+
+import {
+  to = module.lau-case-db-flexible.azurerm_postgresql_flexible_server_configuration.pgsql_server_config["azure.extensions"]
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/lau-case-backend-flexible-data-demo/providers/Microsoft.DBforPostgreSQL/flexibleServers/lau-case-backend-flexible-demo/configurations/azure.extensions"
+}
