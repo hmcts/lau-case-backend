80c0e90f4c780b606e3e8a2c38be4144e25262ab
Priyanka Verma <priyanka.verma@hmcts.net>
2025-08-06 09:33:37 +0100

    logging in retry config

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/feign/RetryableFeignConfig.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/feign/RetryableFeignConfig.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.laubackend.cases.feign;
 
 import feign.Retryer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+@Slf4j
 @Configuration
 public class RetryableFeignConfig {
 
@@ -20,6 +22,8 @@ public class RetryableFeignConfig {
         // Default backoff calculated using the following formula:
         // Math.min(period * Math.pow(1.5, attempt - 1), maxPeriod)
         // First retry will be after 30s, second after 45s, third after 67.5s
+        log.info("Feign retryer configured with initial delay: {} seconds, max delay: {} seconds, max retries: {}",
+                initialDelay, maxDelay, maxRetries);
         return new Retryer.Default(
             SECONDS.toMillis(initialDelay),
             SECONDS.toMillis(maxDelay),
