2a5b79a23f4bcede65ab6bc57bafb942d93b5289
Priyanka Verma <priyanka.verma@hmcts.net>
2026-03-06 13:12:25 +0000

    LAU-1374 refactor delete end point

--- src/main/java/uk/gov/hmcts/reform/laubackend/cases/config/RestApiPreInvokeConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/cases/config/RestApiPreInvokeConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import uk.gov.hmcts.reform.laubackend.cases.authorization.RestApiPreInvokeDeleteInterceptor;
 import uk.gov.hmcts.reform.laubackend.cases.authorization.RestApiPreInvokeInterceptor;
 
 @Configuration
@@ -13,17 +12,10 @@ public class RestApiPreInvokeConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(restApiPreInvokeInterceptor()).addPathPatterns("/audit/**");
-        registry.addInterceptor(restApiPreInvokeDeleteInterceptor())
-                .addPathPatterns("/audit/caseSearch/deleteCaseSearchRecord/**");
     }
 
     @Bean
     public RestApiPreInvokeInterceptor restApiPreInvokeInterceptor() {
         return new RestApiPreInvokeInterceptor();
-    }
-
-    @Bean
-    public RestApiPreInvokeDeleteInterceptor restApiPreInvokeDeleteInterceptor() {
-        return new RestApiPreInvokeDeleteInterceptor();
     }
 }
