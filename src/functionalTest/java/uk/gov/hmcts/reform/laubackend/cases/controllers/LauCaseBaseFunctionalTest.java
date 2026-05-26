c969a8db9a9f5447b05b58d8cd6ba58b67c7391c
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-05-26 15:10:46 +0100

    upgrade serenity

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/controllers/LauCaseBaseFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/cases/controllers/LauCaseBaseFunctionalTest.java
@@ -3,34 +3,25 @@ package uk.gov.hmcts.reform.laubackend.cases.controllers;
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 import lombok.extern.slf4j.Slf4j;
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.serenitybdd.annotations.WithTag;
-import net.serenitybdd.annotations.WithTags;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.BeforeEach;
 import uk.gov.hmcts.reform.laubackend.cases.client.LauCaseBackEndServiceClient;
 
-@RunWith(SpringIntegrationSerenityRunner.class)
-@WithTags({@WithTag("testType:Functional")})
-@ComponentScan("uk.gov.hmcts.reform.laubackend.cases")
-@TestPropertySource("classpath:application-functional.yaml")
 @Slf4j
 @SuppressWarnings({"PMD.AbstractClassWithoutAbstractMethod"})
 public abstract class LauCaseBaseFunctionalTest {
 
-    @Value("${targetInstance}")
+    private static final String DEFAULT_TARGET_INSTANCE = "http://localhost:4550";
+
     protected String lauCaseBackEndApiUrl;
 
     protected LauCaseBackEndServiceClient lauCaseBackEndServiceClient;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured.defaultParser = Parser.JSON;
 
+        lauCaseBackEndApiUrl = System.getenv().getOrDefault("TEST_URL", DEFAULT_TARGET_INSTANCE);
         lauCaseBackEndServiceClient = new LauCaseBackEndServiceClient(lauCaseBackEndApiUrl);
     }
 }
